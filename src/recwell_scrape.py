7a16cfece4e081a6067ffb4693264714145b3fcb
ethan <ethanlim99@gmail.com>
2021-01-28 13:52:32 -0600

    fixed duplicate data issue

--- src/recwell_scrape.py
+++ src/recwell_scrape.py
@@ -1,30 +1,30 @@
 import requests
+import time
 import re
 from bs4 import BeautifulSoup
 
 URL = 'https://services.recwell.wisc.edu/FacilityOccupancy'
 
-main_content = requests.get(URL).text
-soup = BeautifulSoup(main_content, 'html.parser')
-
 # This functions checks The Nick's live building usage
 # RETURN: The percent of occupancy as an integer, returns -1
 # if an exception occurs.
 def checkOccupancy():
+    main_content = requests.get(URL).text
+    soup = BeautifulSoup(main_content, 'html.parser')
     try:
         page_content = soup.find('p', class_='occupancy-count')
+        print(page_content)
         if page_content is None:
             # if for some reason the percent is not available,
             # check the chart's data ratio
-            occupancy_num = checkChart()
+            occupancy_num = checkChart(soup)
         else: 
             occupancy_percentage = page_content.text.strip()
             occupancy_num = re.search(r'(\d+)', occupancy_percentage).group(0)
+            print('checkOccupancy(): ' + str(occupancy_num) + '%')
     except:
         print('Error in checkOccupancy()')
         occupancy_num = -1
-
-    print('Curr occupancy: ' + str(occupancy_num) + '%')
     return int(occupancy_num)
 
 # This function is the "back up" to checkOccupancy
@@ -37,14 +37,15 @@ def checkOccupancy():
 # RETURN: the occupancy percntage as an integer from
 # 'data-ratio', if the whole site is down (it happens)
 # will return -1
-def checkChart():
+def checkChart(soup):
     try:
         chart_content = soup.find('canvas', class_='occupancy-chart').attrs
+        print(chart_content)
         data_ratio = chart_content['data-ratio']
-        ratio_num = float(data_ratio) * 100   
+        ratio_num = float(data_ratio) * 100  
         occupancy_num = int(ratio_num)
+        print('checkChart(): ' + str(occupancy_num) + '%')
     except:
         print('Error in checkChart()')
         occupancy_num = -1
     return occupancy_num
-
