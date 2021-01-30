7cbf5409a96d4f5204f1bd3ef1397ee9fd07e249
ethan <ethanlim99@gmail.com>
2021-01-29 18:22:13 -0600

    removed variable DAILY_UPDATE, fixed issue where no update would be pushed if naptime exceed TWEET_LIMITER

--- src/recwell_bot.py
+++ src/recwell_bot.py
@@ -24,7 +24,6 @@ class Opening(Enum):
     Monday = 6
     Tuesday = 6
     Wednesday = 6
-
     Thursday = 6
     Friday = 6
     Saturday = 10
@@ -46,25 +45,28 @@ class Closing(Enum):
 # GLOBAL VARS
 # IS_OPEN: True if operating during Open Hours, False otherwise
 # REST: Duration of sleep (15s)
-# TWEET_LIMITER: 
+# TWEET_LIMITER: Limits the Tweet rate to 1 Tweet / 10 minutes
+#               If under threshold
 # UPDATED_OPEN: True if profile has been updated to open
 #               default is True to prevent unwanted updates
 #               when redeploying/restarting container
 # UPDATED_CLOSE: True if profile has been updated to close
 #               default is True to prevent unwanted updates
 #               when redploying/restarting container
-# OPENING_HOURS: List of Enum values that correspond to opening and closing hours
-# DAILY_TOTAL: Keeps track of total number of tweets posted in a day. 
-#                Used to circumvent duplicate status error. Reset to 0 at close
+
 IS_OPEN = None
 UPDATED_OPEN = False
 UPDATED_CLOSE = False
 REST = 15
 TWEET_LIMITER =  4 * 10
-DAILY_TOTAL = 0
+
+
+# OPENING_HOURS: List of Enum values that correspond to opening hours
+# CLOSING_HOURS: List of Enum values that correspond to closing hours
 OPENING_HOURS = [
                 Opening.Monday.value, 
-                Opening.Tuesday.value, 
+                Opening.Tuesday.value,
+                Opening.Wednesday.value,  
                 Opening.Thursday.value,
                 Opening.Friday.value,
                 Opening.Saturday.value,
@@ -72,7 +74,8 @@ OPENING_HOURS = [
                 ]    
 CLOSING_HOURS = [
                 Closing.Monday.value, 
-                Closing.Tuesday.value, 
+                Closing.Tuesday.value,
+                Closing.Wednesday.value, 
                 Closing.Thursday.value,
                 Closing.Friday.value,
                 Closing.Saturday.value,
@@ -96,7 +99,7 @@ def getCurrHour():
 # i.e. Monday = 0, Tuesday = 1, ... Sunday = 6
 # RETURN: Current weekday as integer
 def getCurrDay():
-    curr_day = datetime.today().weekday()
+    curr_day = datetime.today().weekday() 
     return int(curr_day)
 
 # Function that setups globals for open hours and updates the
@@ -105,6 +108,8 @@ def openSetup():
     global IS_OPEN, UPDATED_OPEN, UPDATED_CLOSE
     IS_OPEN = True
     if not UPDATED_OPEN:
+        # Tweet an Open status and
+        # change profile to Open
         handleOpenUpdate(api)
         handleOpenProfile(api)
         UPDATED_OPEN = True
@@ -113,12 +118,11 @@ def openSetup():
 # Function that setups globals for close hours and updates the
 # profile to convey The Nick is closed (only updates profile once)
 def closedSetup():
-    global IS_OPEN, UPDATED_OPEN, UPDATED_CLOSE, DAILY_TOTAL
+    global IS_OPEN, UPDATED_OPEN, UPDATED_CLOSE
     IS_OPEN = False
     if not UPDATED_CLOSE:
         UPDATED_CLOSE = True
         UPDATED_OPEN = False # reset for open update later
-        DAILY_TOTAL = 0
         handleClosedUpdate(api)
         handleClosedProfile(api)
 
@@ -139,18 +143,19 @@ while True:
     try:
         curr_hour = getCurrHour()
         curr_day = getCurrDay()
+
         # Operates normally during The Nick's open and close hours
         if OPENING_HOURS[curr_day] <= curr_hour < CLOSING_HOURS[curr_day]:
-            curr_occupancy = checkOccupancy()
             openSetup()
+            curr_occupancy = checkOccupancy()
             update_naptime += 1
-            # checkOccupancy returns -1 if exception occurs
-            # if the current capacity is under acceptable threshold (95%)
-            # and it's been 10 minutes
-            if 0 <= curr_occupancy <= 95 and update_naptime == TWEET_LIMITER:
-                DAILY_TOTAL += 1
-                handleUpdates(api, curr_occupancy, DAILY_TOTAL)
+    
+            # if occupancy is under threshold (95%) and no erro (-1) and it has 
+            # been more than 10 minutes since last Tweet -> Push Tweet
+            if 0 <= curr_occupancy <= 95 and update_naptime >= TWEET_LIMITER:
                 update_naptime = 0
+                handleUpdates(api, curr_occupancy)
+
             handleMentions(api, curr_occupancy, IS_OPEN)
         # If outside of open hours, will not post new updates and
         # responds to mentions with special response
