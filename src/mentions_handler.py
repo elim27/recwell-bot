42cf4d2a9ee61a42d9b4a741e49f75b39d0efaae
ethan <ethanlim99@gmail.com>
2021-02-01 18:58:34 -0600

    added logging, included logic to handle backup data in fullHandler

--- src/mentions_handler.py
+++ src/mentions_handler.py
@@ -1,11 +1,19 @@
+import logging
 import tweepy
 from decouple import config
+from recwell_scrape import checkOccupancy
 
-FILE_NAME = config('FILE_NAME', cast=str)
+FILE_NAME = config('FILE_PATH_ID', cast=str)
 SCREEN_NAME = config('SCREEN_NAME')
 BANNED_WORDS = []
 SEEN_IDS = []
 
+
+logging.basicConfig(filename='/var/log/handlers.log', 
+                    filemode='a', 
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO
+)
 # Function initializes BANNED_WORDS dictionary from words defined
 # in .env
 def setupBannedWords():
@@ -27,7 +35,7 @@ def getLastID(file_name):
     except ValueError:
         last_id = 0
     except FileNotFoundError:
-        print('FileNotFoundError :(')
+        logging.error('FileNotFoundError :(')
 
     return last_id
 
@@ -41,7 +49,9 @@ def storeLastID(file_name, last_id):
         fw.write(str(last_id))
         fw.close()
     except FileNotFoundError:
-        print(FILE_NAME + ' not found :(')
+        logging.info(FILE_NAME + ' not found :(')
+    except:
+        logging.info('error in storeLastID')
     
 # Function handles response if user mentions but with '#full'
 # If the occupancy is at 100%, the bot responds 'Yes.', if an
@@ -52,26 +62,32 @@ def storeLastID(file_name, last_id):
 # tweet: Status object that needs to be responded to
 # curr_occupancy: current occupancy of The Nick as an integer
 def fullHandler(api, tweet, curr_occupancy):
-    print('fullHandler() called')
+    logging.info('fullHandler() called')
     try:
         curr_percent = 'Current Occupancy: ' + str(curr_occupancy) + '%'
-
+        backup_data = checkOccupancy()['BACKUP_DATA']
+        # Attach notice if curr_occupancy is from backup data
+        if backup_data:
+            curr_percent += ('\n NOTE: Recwell Services down, using backup '
+                            + 'data... Results may not be up to date!')
+        # At capacity scenario
         if curr_occupancy == 100:
             api.update_status('@' + tweet.user.screen_name + ' Yes. \U0001F614\n'
                             + curr_percent, in_reply_to_status_id=tweet.id)
+        # Error scenario                   
         elif curr_occupancy == -1:
             api.update_status('@' + tweet.user.screen_name + ' Uh oh, it '
                             + 'seems like my source is currently down '
-                            + '\U0001F62C \U0001F6E0\n Try this alternate link!', 
-                            attachment_url='https://recwell.wisc.edu/liveusage/',
+                            + '\U0001F62C \U0001F6E0\n ',
                             in_reply_to_status_id=tweet.id)
-        else:
+        # Normal scenario
+        elif curr_occupancy != 100:
             api.update_status('@' + tweet.user.screen_name + ' Nope! \U0001F601\n'
                             + curr_percent, in_reply_to_status_id=tweet.id)
-        print('Response to #full successful!')
-    except tweepy.TweepError as e:
-        print('Response to #full failed!')
-        print(e.reason)
+        logging.info('Response to #full successful!')
+    except Exception as e:
+        logging.error('Response to #full failed!')
+        logging.error(e)
 
 # This function takes care of favoriting tweets
 # that compliment the bot with the phrase 'good bot'
@@ -79,10 +95,11 @@ def fullHandler(api, tweet, curr_occupancy):
 # tweet: Status object that needs to be responded to
 def favHandler(api, tweet):
     try:
-        api.create_favorte(tweet.id)
-        print('favorite successful')
-    except tweepy.TweepError:
-        print('favorite failed!')
+        api.create_favorite(tweet.id)
+        logging.info('favorite successful')
+    except Exception as e:
+        logging.info('favorite failed!')
+        logging.info(e)
 
 # Function that handles responding to tweets that mention the bot
 # but it is outside of open hours (meaning it doesn't make sense to
@@ -95,10 +112,10 @@ def closedHandler(api, tweet):
         api.update_status('@' + tweet.user.screen_name + ' The Nick is currently '
                         + 'closed \U0001F634 \U0001F319', 
                         in_reply_to_status_id=tweet.id)
-        print('Closed Response to #full successful!')
-    except tweepy.TweepError as e:
-        print('Closed Response to #full failed!')
-        print(e.reason)
+        logging.info('Closed Response to #full successful!')
+    except Exception as e:
+        logging.info('Closed Response to #full failed!')
+        logging.info(e)
 
 # Function that handles responding to tweets that mention the bot and 
 # use a specific hashtag or phrase. Saves Tweet ID into a text file (last_id.txt) 
@@ -112,36 +129,38 @@ def closedHandler(api, tweet):
 # api: api object that gives access to Twitter's REST API methods
 # curr_occupancy: current occupancy of The Nick as an integer
 # IS_OPEN: True when The Nick is open, False otherwise
-def handleMentions(api, curr_occupancy, open):
-    print('handleMentions() called')
-    setupBannedWords()
-    last_id = getLastID(FILE_NAME)
+def handleMentions(api, open):
     try:
+        setupBannedWords()
+        curr_occupancy = checkOccupancy()['occupancy_num']
+        last_id = getLastID(FILE_NAME)
+        # Retrieve mentions  
         mentions = api.mentions_timeline(last_id, tweet_mode='extended')
-    except tweepy.TweepError:
-        print('Error, unable to get mentions')
-        return
+        # Responds to tweets from oldest to newest
+        for tweet in reversed(mentions):
+            logging.info(str(tweet.id))
+            
+            last_id = tweet.id # ID of last tweet seen
+            storeLastID(FILE_NAME, last_id)
+           
+            # convert the mention text to lower case
+            tweet_text = tweet.full_text.lower()
 
-    # Responds to tweets from oldest to newest
-    for tweet in reversed(mentions):
-        print('Reading tweets...')
-        print(str(tweet.id))
-        last_id = tweet.id # ID of last tweet seen
-        storeLastID(FILE_NAME, last_id)
-        tweet_text = tweet.full_text.lower()
-
-        # Make sure the bot doesn't respond to its own tweets
-        # Ensure the bot doesn't respond to words that are in BANNED_WORDS
-        # Make sure The Nick is open and not closed
-        if (SCREEN_NAME not in tweet.user.screen_name and
-                        any(word not in tweet_text for word in BANNED_WORDS)):
-            if open:
-                if '#full' in tweet_text:
-                    fullHandler(api, tweet, curr_occupancy)
-                if 'good bot' in tweet_text:
-                    favHandler(api, tweet)
+            # Make sure the bot doesn't respond to its own tweets
+            # Ensure the bot doesn't respond to words that are in BANNED_WORDS
+            # Make sure The Nick is open and not closed
+            if (SCREEN_NAME not in tweet.user.screen_name and
+                            any(word not in tweet_text for word in BANNED_WORDS)):
+                if open:
+                    if '#full' in tweet_text:
+                        fullHandler(api, tweet, curr_occupancy)
+                    if 'good bot' in tweet_text:
+                        favHandler(api, tweet)
+                else:
+                    closedHandler(api, tweet)
             else:
-                closedHandler(api, tweet)
-        else:
-            print('Tweet ignored')
+                logging.info('Tweet ignored')
+    except Exception as e:
+            logging.info('Error, unable to get mentions')
+            logging.info(e)
 
