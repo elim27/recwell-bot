1ace7de710a265a924502bfd6bfabbd79d1c16e1
ethan <ethanlim99@gmail.com>
2021-02-01 18:58:56 -0600

    added logging

--- src/profile_handler.py
+++ src/profile_handler.py
@@ -1,29 +1,34 @@
+import logging
 import tweepy
 
-# This function updates the profile of the bot to an 'open' status
+logging.basicConfig(filename='/var/log/handlers.log', 
+                    filemode='a', 
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO
+)
+# This function updates the profile of the bot to an 'open' or 'closed'
+# status based on if the building is open or not.
 # PARAMS:
 # api: api object that gives access to Twitter's REST API methods
-def handleOpenProfile(api):
+# IS_OPEN: True if The Nick is open, False otherwise
+def handleProfile(api, IS_OPEN):
     try:
-        new_description = ('The Nick is currently OPEN \U00002600 '
-                        + 'I tweet out every 10 minutes if The Nick is NOT full. '
-                        + 'Mention me with #full for a live response \U0001F60E ')
-        api.update_profile(name='is the nick full ? \U0001F914', 
+        # If The Nick is open, switch the OPEN/CLOSED text and emoji
+        if IS_OPEN:
+            open_closed_indicator = 'OPEN \U00002600'
+            name_emoji = '\U0001F914' # thinking face
+        else:
+            open_closed_indicator = 'CLOSED \U0001F4A4'
+            name_emoji = '\U0001F634' # sleeping face
+        
+        new_description = ('The Nick is currently ' + open_closed_indicator 
+                        + ' I tweet out the current occupancy of The Nick ' 
+                        + 'every 25 minutes | Mention me with #full for a live' 
+                        + ' response \U0001F60E')
+        api.update_profile(name='is the nick full ? ' + name_emoji,
                         description=new_description)
-        print('OPEN profile updated succesfully!')
+        logging.info('Profile updated successfully!')
     except tweepy.TweepError:
-        print('Error, profile OPEN FAILED!')
+        logging.error('Error, profile update FAILED!')
+        
 
-# This function updates the profile of the bot to a 'closed' status
-# PARAMS:
-# api: api object that gives access to Twitter's REST API methods
-def handleClosedProfile(api):
-    try:
-        new_description = ('The Nick is currently CLOSED \U0001F4A4 '
-                        +'I tweet out every 10 minutes if The Nick is NOT full. '
-                        + 'Mention me with #full for a live response \U0001F60E ')
-        api.update_profile(name='is the nick full ? \U0001F914', 
-                        description=new_description)
-        print('CLOSED profile updated succesfully!')
-    except tweepy.TweepError:
-        print('Error, profile CLOSE FAILED!')
