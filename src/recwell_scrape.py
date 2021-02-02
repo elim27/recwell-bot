d5cd661e698c97fa38aca421ef3127db1179c0b4
ethan <ethanlim99@gmail.com>
2021-02-01 19:04:51 -0600

    changed return value of checkOccupancy to dict, added a call to checkBackup() (selenium scraper) when error occurs in checkChart(), added global boolean BACKUP_DATA indicating if sellenium scraper was used or not

--- src/recwell_scrape.py
+++ src/recwell_scrape.py
@@ -1,19 +1,31 @@
+import logging
+import re
 import requests
 import time
-import re
 from bs4 import BeautifulSoup
+from backup_scrape import checkBackup
 
+# Global Vars
+# URL: source for web scraping
+# BACKUP_DATA: If using backup data from less accurate reserve
+#               BACKUP_DATA -> True
 URL = 'https://services.recwell.wisc.edu/FacilityOccupancy'
+BACKUP_DATA = False
 
+logging.basicConfig(filename='/var/log/scrapes.log', 
+                    filemode='a', 
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO
+)
 # This functions checks The Nick's live building usage
 # RETURN: The percent of occupancy as an integer, returns -1
 # if an exception occurs.
 def checkOccupancy():
+    global BACKUP_DATA
     main_content = requests.get(URL).text
     soup = BeautifulSoup(main_content, 'html.parser')
     try:
         page_content = soup.find('p', class_='occupancy-count')
-        print(page_content)
         if page_content is None:
             # if for some reason the percent is not available,
             # check the chart's data ratio
@@ -21,31 +33,39 @@ def checkOccupancy():
         else: 
             occupancy_percentage = page_content.text.strip()
             occupancy_num = re.search(r'(\d+)', occupancy_percentage).group(0)
-            print('checkOccupancy(): ' + str(occupancy_num) + '%')
+            BACKUP_DATA = False
+
+        data = {'occupancy_num': occupancy_num, 'BACKUP_DATA': BACKUP_DATA} 
+        logging.info('Occupancy num: '+ str(occupancy_num))
+        return data
     except:
-        print('Error in checkOccupancy()')
-        occupancy_num = -1
-    return int(occupancy_num)
+        logging.info('Error in checkOccupancy()')
+
 
 # This function is the "back up" to checkOccupancy
 # Sometimes the source website tweaks and the percentage
 # disappears. However, to get the percentage, there is an
 # HTML attribute called 'data-ratio' for the graph
-#  which holds the percentage as a ratio (decimal).
+# which holds the percentage as a ratio (decimal).
 # Simply type casting as a float and multiplying a 100 and 
 # then type cast to int solves the problem
 # RETURN: the occupancy percntage as an integer from
-# 'data-ratio', if the whole site is down (it happens)
-# will return -1
+# 'data-ratio'
 def checkChart(soup):
+    global BACKUP_DATA
+    BACKUP_DATA = False
     try:
         chart_content = soup.find('canvas', class_='occupancy-chart').attrs
-        print(chart_content)
         data_ratio = chart_content['data-ratio']
         ratio_num = float(data_ratio) * 100  
         occupancy_num = int(ratio_num)
-        print('checkChart(): ' + str(occupancy_num) + '%')
     except:
-        print('Error in checkChart()')
-        occupancy_num = -1
+        logging.error('Error in checkChart()')
+        # If the whole website is down,
+        # It will access the backup backup
+        # website with live but less reliable data
+        # BACKUP_DATA set to False since the data is not as reliable
+        BACKUP_DATA = True
+        occupancy_num = checkBackup()    
     return occupancy_num
+
